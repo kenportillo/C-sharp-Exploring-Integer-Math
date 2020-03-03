8c1ec5f9488a3c5f67cec514fe873ddad80b01a0
github username <github email address>
2020-03-02 23:29:51 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exploring_Integer_Math
+{
+    class Program
+    {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+
+            // subtraction
+            c = a - b;
+            Console.WriteLine(c);
+            
+            // multiplication
+            c = a * b;
+            Console.WriteLine(c);
+
+            // division
+            c = a / b;
+            Console.WriteLine(c);
+
+            // Experiment Mathematics
+            c = a + b - 12 * 17;
+            Console.WriteLine(c);
+
+            // Console.WriteLine("Hello World!");
+        }
+
+        static void Main(string[] args)
+        {
+            // WorkingWithIntegers();
+
+            // The output demonstrates that the multiplication is performed
+            // before the addition.
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+        }
+    }
+}
